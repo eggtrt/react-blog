70121ebc788b8fd997206be2507f0de5272f6130
jisu <eggtrt@gmail.com>
2021-05-12 21:06:09 +0900

    user model

---
+++ server/models/post.js
@@ -0,0 +1,49 @@
+import moment from 'moment';
+import mongoose, { mongo } from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["MainJuin", "Subjuin", "User"],
+        default: "User"
+    },
+    register_date: {
+        type: Date,
+        default: moment().format("YYYY-MM-DD")
+    },
+    comments: [
+        {
+            post_id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "posts"
+            },
+            comment_id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "comments"
+            }
+        }
+    ],
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "posts"
+        }
+    ]
+});
+
+const User = mongoose.model("user", UserSchema);
+
+export default User;
