e7eff07ddca4aa7abe9bf984c2007e0c54b513e9
jisu <eggtrt@gmail.com>
2021-05-13 20:37:20 +0900

    connect mongo DB

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
 
 const app = express()
+const { MONGO_URI } = config;
 
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true
+}).then(() => console.log("Mongo DB connect"))
+.catch((e) => console.error(e));
 app.get('/');
 
 export default app;
